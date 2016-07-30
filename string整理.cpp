1fda2d27a23d0bf4763f78c35fce12cb27048315
fushuyue <fushuyue_apply@hotmail.com>
2016-07-30 14:30:33 +0800

    some codes for manipulating strings and char

---
+++ string整理.cpp
@@ -0,0 +1,19 @@
+// 复制b的前c个字符到a
+memcpy(char *a, char *b, int c)
+
+// 将string转换成char
+const char *thisname = name.c_str();
+
+// change int into string
+std::string int_to_string(int a)
+{
+	std::stringstream ss;
+	ss << a;
+	std::string b;
+	ss >> b;
+	return b;
+}
+
+
+//
+
